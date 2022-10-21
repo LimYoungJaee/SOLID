a10b8d5cb455acb481fb8ce7fafd633f2a580f55
LimYoungJaee <109709299+LimYoungJaee@users.noreply.github.com>
2022-10-21 15:10:30 +0900

    Create BadOrderService.cs

---
+++ SRP/Bad/BadOrderService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SRP01
+{
+    internal class BadOrderService
+    {
+        public void CreateOrder()
+        {
+            //CreateOrder()
+            //MakePayment()
+            //GenerateInvoice()
+            //EmailInvoice()
+        }
+    }
+}
